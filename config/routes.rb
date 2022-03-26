11687b4d81e27631244e202cd1d2411ab0bfbb4e
tomoko <tomoko@iMac.local>
2022-03-26 22:07:37 +0900

    提出用 first commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'profiles/new'
+  root 'top_pages#home'
+  devise_for :users, controllers: {registrations: 'users/registrations',sessions: 'users/sessions',}
+  resources :users
+  resources :rooms
+  resources :reservations
+  resources :profiles
+  get 'search' => 'rooms#search'
+  get 'area' => 'rooms#area'
 end
